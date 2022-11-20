23849c483d591d44bde005fcfdee344209256279
Medokins <nikodemszwast@gmail.com>
2022-11-20 21:23:01 +0100

    fix Exercise_1

--- Lab04/main.py
+++ Lab04/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import time
+import matplotlib.pyplot as plt
 
 EPSILON = 1
 DELTA = .1
@@ -27,9 +28,8 @@ def relaksacja_globalna(omega_g):
     ro = fill_ro()
 
     for omega in omega_g:
-        start = time.time()
-        vs = np.zeros((N_X + 1, N_Y + 1))
-        vn = np.zeros((N_X + 1, N_Y + 1))
+        vs = np.zeros((N_X, N_Y))
+        vn = np.zeros((N_X, N_Y))
         vs[:, 0] = V_1
         vn[:, 0] = V_1
         s_prev = 1
@@ -38,44 +38,57 @@ def relaksacja_globalna(omega_g):
         while True:
             vn[1:N_X-1, 1:N_Y-1] = .25 * (vs[2:N_X, 1:N_Y-1] + vs[:N_X-2, 1:N_Y-1] + vs[1:N_X-1, 2:N_Y] + vs[1:N_X-1, :N_Y-2] + np.square(DELTA) * ro[1:N_X-1, 1:N_Y-1] / EPSILON)
             vn[0, 1:N_Y-1] = vn[1, 1:N_Y-1]
-            vn[N_X, 1:N_Y-1] = vn[N_X-1, 1:N_Y]
+            vn[N_X-1, 1:N_Y-1] = vn[N_X - 2, 1:N_Y - 1]
 
             vs = np.add(np.multiply((1.0 - omega), vs), np.multiply(omega, vn))
             s_prev = s_next
             s_next = 0
-            s_next += np.sum(np.square(DELTA) * ((.5 * np.square((vs[1:N_X, :N_Y-1] - vs[:N_X-1, :N_Y-1]) / DELTA)) + .5 * np.square((vs[:N_X-1, 1:N_Y] - vs[:N_X-1, :N_Y-1]) / DELTA) - ro[:N_X-1, :N_Y-1] * vs[:N_X-1, :N_Y-1]))
+            s_next += np.sum(np.square(DELTA) * ((.5 * np.square((vs[1:, :N_Y-1] - vs[:N_X-1, :N_Y-1]) / DELTA)) + .5 * np.square((vs[:N_X-1, 1:] - vs[:N_X-1, :N_Y-1]) / DELTA) - ro[:N_X-1, :N_Y-1] * vs[:N_X-1, :N_Y-1]))
 
-            if abs((s_next - s_prev) / s_prev) < TOL: break
+            if abs((s_next - s_prev) / s_prev) < TOL:
+                break
+    
+        error = np.zeros((N_X, N_Y))
+        error[1:N_X-1, 1:N_Y-1] = (vn[2:, 1:N_Y-1] - 2.0 * vn[1:N_X-1, 1:N_Y-1] + vn[:N_X-2, 1:N_Y-1]) / DELTA**2 \
+                                    + (vn[1:N_X-1, 2:] - 2.0 * vn[1:N_X-1, 1:N_Y-1] + vn[1:N_X-1, :N_Y-2]) / DELTA ** 2 \
+                                    + ro[1:N_X-1, 1:N_Y-1] / EPSILON
+     
+        plt.title(f"Global Error: omega={omega}")
+        plt.imshow(error.T, cmap='viridis')
+        plt.colorbar()
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.savefig(f"Global error omega={omega}.png", bbox_inches='tight', transparent=False)
+        plt.clf()
 
-        end = time.time()
-        time_passed = (end - start)
-        print(f"Time elapsed = {time_passed}")
+        plt.title(f"Global: omega={omega}")
+        plt.imshow(vn.T, cmap='viridis')
+        plt.colorbar()
+        plt.savefig(f"Global omega={omega}.png", bbox_inches='tight', transparent=False)
+        plt.clf()
 
 def relaksacja_lokalna(omega_l):
     ro = fill_ro()
 
     for omega in omega_l:
-        start = time.time()
-        v = np.zeros((N_X + 1, N_Y + 1))
-        v[:, 0] = V_1
+        v = np.zeros((N_X, N_Y))
+        v[:, 0] = V_2
         s_prev = np.float64(1)
         s_next = np.float64(1)
 
         while True:
             v[1:N_X-1, 1:N_Y-1] = (1 - omega)*v[1:N_X-1, 1:N_Y-1] + omega * .25 * (v[2:N_X, 1:N_Y-1] + v[:N_X-2, 1:N_Y-1] + v[1:N_X-1, 2:N_Y] + v[1:N_X-1, :N_Y-2] + np.square(DELTA) * ro[1:N_X-1, 1:N_Y-1] / EPSILON)
             v[0, 1:N_Y-1] = v[1, 1:N_Y-1]
-            v[N_X, 1:N_Y-1] = v[N_X-1, 1:N_Y-1]
+            v[N_X-1, 1:N_Y-1] = v[N_X-2, 1:N_Y-1]
 
             s_prev = s_next
             s_next = np.float64(0)
             s_next += np.sum(np.square(DELTA) * ((.5 * np.square((v[1:N_X, :N_Y-1] - v[:N_X-1, :N_Y-1]) / DELTA)) + .5 * np.square((v[:N_X-1, 1:N_Y] - v[:N_X-1, :N_Y-1]) / DELTA) - ro[:N_X-1, :N_Y-1] * v[:N_X-1, :N_Y-1]))
 
-            print(s_next)
-            if abs((s_next - s_prev) / s_prev) < TOL: break
+            print(abs((s_next - s_prev) / s_prev))
+            if abs((s_next - s_prev) / s_prev) < TOL: 
+                break
 
-        end = time.time()
-        time_passed = (end - start)
-        print(f"Time elapsed = {time_passed}")
 
 #relaksacja_globalna([.6, 1])
-relaksacja_lokalna([1.4, 1.8, 1.9])
+#relaksacja_lokalna([1, 1.4, 1.8, 1.9])
